14591116beec81eb139c2c946cf2f37e218ec0b1
Lotraemon <Lotraemon@gmail.com>
2024-11-28 14:01:36 +0300

    upd1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,8 +2,6 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("\n" + "Задание 1:");
-        printThreeWords();
 
         System.out.println("\n" + "Задание 2:");
         checkSumSign();
