6f54b05248a8d3a28642e9ee0cc7246d86a794dc
Lotraemon <Lotraemon@gmail.com>
2024-12-22 13:13:09 +0300

    upd

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -8,6 +8,6 @@ public class Test {
     }
 
     public static void main(String[] args) {
-        System.out.println(calculateFactorial(-5));
+        System.out.println(calculateFactorial(3));
     }
-}//
+}
