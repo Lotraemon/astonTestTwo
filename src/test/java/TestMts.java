85ef2217e28676a19d43b5233b204fe9b297888e
Lotraemon <Lotraemon@gmail.com>
2024-12-25 00:05:49 +0300

    upd

--- src/test/java/TestMts.java
+++ src/test/java/TestMts.java
@@ -1,14 +1,15 @@
+
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class TestMts {
@@ -38,13 +39,42 @@ public class TestMts {
         Assertions.assertEquals("Онлайн пополнение\n" + "без комиссии", textBlock.getText());
     }
 
+    //*[@id='pay-section']//ul/li[1]/img
     @Test
     void testLogos() {
-        String[] altLogos = new String[]{"VISA", "Verifided by VISA",
-                "MasterCard", "MasterCard Secure Code", "Белкарт"};
+        String[] altLogos = new String[]{"Visa", "Verified By Visa", "MasterCard", "MasterCard Secure Code", "Белкарт"};
         driver.get("http://mts.by/");
-        WebElement logo = driver.findElement(By.xpath("//*[@id='pay-section']//ul/li[1]/img"));
-        String altLogo = logo.getAttribute("alt");
-        Assertions.assertEquals("Visa", altLogo);
+        List<WebElement> logos = driver.findElements(By.xpath("//*[@class='pay__wrapper']//ul/li/img"));
+        for (int i = 0; i < altLogos.length; i++) {
+            String altLogo = logos.get(i).getAttribute("alt");
+            Assertions.assertEquals(altLogos[i], altLogo);
+        }
     }
+
+    @Test
+    void testLink() {
+        driver.get("http://mts.by/");
+        WebElement closeCookie = driver.findElement(By.xpath("//*[@class='btn btn_gray cookie__cancel']"));
+        closeCookie.click();
+        WebElement checklLink = driver.findElement(By.linkText("Подробнее о сервисе"));
+        checklLink.click();
+        Assertions.assertTrue(driver.getCurrentUrl().contains("poryadok-oplaty"));
+    }
+
+    @Test
+    void TestForm() {
+        driver.get("http://mts.by/");
+        WebElement closeCookie = driver.findElement(By.xpath("//*[@class='btn btn_gray cookie__cancel']"));
+        closeCookie.click();
+        WebElement phoneNumber = driver.findElement(By.xpath("//*[@class='pay__form']//*[@id='connection-phone']"));
+        phoneNumber.sendKeys("297777777");
+        WebElement connectionSum = driver.findElement(By.xpath("//*[@class='pay__form']//*[@id='connection-sum']"));
+        connectionSum.sendKeys("10");
+        WebElement confirm = driver.findElement(By.xpath("//*[@class='pay__form']//button"));
+        confirm.click();
+        WebElement bepaidApp = driver.findElement(By.xpath("//*[@id='cc-number']"));
+        Assertions.assertTrue(bepaidApp.isDisplayed());
+
+    }
+
 }
