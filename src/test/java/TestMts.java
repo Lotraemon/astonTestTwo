e165b66a42e872bd2f40c59e9ed0b47cc6d7c0e9
Lotraemon <Lotraemon@gmail.com>
2024-12-25 18:25:29 +0300

    upd

--- src/test/java/TestMts.java
+++ src/test/java/TestMts.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -22,7 +21,10 @@ public class TestMts {
     @BeforeEach
     void setupTest() {
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://mts.by/");
+        WebElement closeCookie = driver.findElement(By.xpath("//*[@class='btn btn_gray cookie__cancel']"));
+        closeCookie.click();
     }
 
     @AfterEach
@@ -32,7 +34,6 @@ public class TestMts {
 
     @Test
     void testBlockTitle() {
-        driver.get("http://mts.by/");
         WebElement textBlock = driver.findElement(By.xpath("//*[@class='pay__wrapper']/h2"));
         Assertions.assertEquals("Онлайн пополнение\n" + "без комиссии", textBlock.getText());
     }
@@ -40,7 +41,6 @@ public class TestMts {
     @Test
     void testLogos() {
         String[] altLogos = new String[]{"Visa", "Verified By Visa", "MasterCard", "MasterCard Secure Code", "Белкарт"};
-        driver.get("http://mts.by/");
         List<WebElement> logos = driver.findElements(By.xpath("//*[@class='pay__wrapper']//ul/li/img"));
         for (int i = 0; i < altLogos.length; i++) {
             String altLogo = logos.get(i).getAttribute("alt");
@@ -50,9 +50,6 @@ public class TestMts {
 
     @Test
     void testLink() {
-        driver.get("http://mts.by/");
-        WebElement closeCookie = driver.findElement(By.xpath("//*[@class='btn btn_gray cookie__cancel']"));
-        closeCookie.click();
         WebElement checklLink = driver.findElement(By.linkText("Подробнее о сервисе"));
         checklLink.click();
         Assertions.assertTrue(driver.getCurrentUrl().contains("poryadok-oplaty"));
@@ -60,17 +57,12 @@ public class TestMts {
 
     @Test
     void TestForm() {
-        driver.get("http://mts.by/");
-        WebElement closeCookie = driver.findElement(By.xpath("//*[@class='btn btn_gray cookie__cancel']"));
-        closeCookie.click();
         WebElement phoneNumber = driver.findElement(By.xpath("//*[@class='pay__form']//*[@id='connection-phone']"));
         phoneNumber.sendKeys("297777777");
         WebElement connectionSum = driver.findElement(By.xpath("//*[@class='pay__form']//*[@id='connection-sum']"));
         connectionSum.sendKeys("10");
         WebElement confirm = driver.findElement(By.xpath("//*[@id='pay-connection']/button"));
         confirm.click();
-        //WebDriverWait wait = new WebDriverWait(driver, 10);
-        //WebElement iframe = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("iframe.bepaid-iframe")));
         WebElement iframe = driver.findElement(By.cssSelector("iframe.bepaid-iframe"));
         driver.switchTo().frame(iframe);
         WebElement bepaidApp = driver.findElement(By.xpath("//*[@id='cc-number']"));//cc-number
